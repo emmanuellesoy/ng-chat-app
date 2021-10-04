349c0fa702823dd5b30300386863d3a0fb008f34
Hira Kafle <16020447+hiradadiamond@users.noreply.github.com>
2021-10-04 10:19:52 -0500

    Added app component

--- ngChatApp/src/app/app-routing.module.ts
+++ ngChatApp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatViewComponent } from './chat/chat-view/chat-view.component';
+import { RegisterViewComponent } from './register/register-view/register-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterViewComponent },
+  {path:'chat', component:ChatViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
